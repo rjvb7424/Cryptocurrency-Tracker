7febc6ed58a1792f2ad318e6f898a7fdb8bfbae5
rjvb7424 <rjvb7424@leeds.ac.uk>
2024-12-06 14:59:55 +0000

    The software now fetches crypto data from the API organzizes it selectes the data and prints onto a window the prices of several crypto currencies.

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 import requests
+import tkinter as tk
 
 url = "http://api.coinlayer.com/"
 api_key = "9c34686dbd6c309b03fa6cc0c6399dc2"
 
 def get_crypto_data():
-    crypto_data = requests.get(url + "live?access_key=" + api_key).json()  # Get the data from the API
+    crypto_data = requests.get(url + "live?access_key=" + api_key).json()
     return crypto_data
 
 def organize_crypto_data(crypto_data):
-    organized_data = {}  # Create an empty dictionary to store the organized data
-    for crypto in crypto_data['rates']:  # Loop through the rates in the data
-        organized_data[crypto] = crypto_data['rates'][crypto]  # Add the rate to the dictionary
+    organized_data = {} 
+    for crypto in crypto_data['rates']:
+        organized_data[crypto] = crypto_data['rates'][crypto]
     return organized_data
+
+def select_crypto_data(organized_data):
+    selected_data = {}
+    selected_cryptos = ['BTC', 'ETH', 'XRP', 'LTC', 'BCH']
+    for crypto in selected_cryptos:
+        selected_data[crypto] = organized_data[crypto]
+    return selected_data
+
+root = tk.Tk()
+root.title("Crypto Prices")
+root.geometry("400x200")
+
+crypto_data = get_crypto_data()
+organized_data = organize_crypto_data(crypto_data)
+selected_data = select_crypto_data(organized_data)
+
+bitcoin_label = tk.Label(root, text="Bitcoin: $" + str(selected_data['BTC']))
+bitcoin_label.pack()
+ethereum_label = tk.Label(root, text="Ethereum: $" + str(selected_data['ETH']))
+ethereum_label.pack()
+xrp_label = tk.Label(root, text="XRP: $" + str(selected_data['XRP']))
+xrp_label.pack()
+litecoin_label = tk.Label(root, text="Litecoin: $" + str(selected_data['LTC']))
+litecoin_label.pack()
+bitcoin_cash_label = tk.Label(root, text="Bitcoin Cash: $" + str(selected_data['BCH']))
+bitcoin_cash_label.pack()
+
+root.mainloop()
